c255d550177628d758cc584266d062adc113f9d4
dianestephani <diane.stephani@gmail.com>
2020-06-22 23:53:18 -0700

    background image and search bar intial styling

---
+++ diane/script.js
@@ -0,0 +1,13 @@
+// function generator(){
+//   let x = Math.floor((Math.random()*6)+ 1);
+//   console.log(x);
+//   document.getElementById('#divImage').innerHTML = 
+//     `<img src="images/city${x}.jpeg">`;
+// }
+
+// $(document).ready(generator());
+
+// window.addEventListener('load', (event) => {
+//   console.log('The page has fully loaded');
+//   generator();
+// });
