237dfe8ef305b65bba653818bad59b1895b145e4
dianestephani <diane.stephani@gmail.com>
2020-06-23 15:20:48 -0700

    images

--- diane/script.js
+++ diane/script.js
@@ -11,3 +11,17 @@
 //   console.log('The page has fully loaded');
 //   generator();
 // });
+
+// For news API based on keyword
+// var url = 'http://newsapi.org/v2/everything?' +
+//           'q=Apple&' +
+//           'from=2020-06-23&' +
+//           'sortBy=popularity&' +
+//           'apiKey=55dfbfb6602448b1b3f17649cbece724';
+
+// var req = new Request(url);
+
+// fetch(req)
+//     .then(function(response) {
+//         console.log(response.json());
+//     })
