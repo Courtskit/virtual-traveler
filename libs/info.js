56e14dd5b4b4b8abb68e1acc6d597cadbf341aa8
AndrewCS149 <andrewcs149@gmail.com>
2020-06-24 16:53:56 -0700

    Add parks api and insert logic into promise.all.

--- libs/info.js
+++ libs/info.js
@@ -30,6 +30,12 @@ function Photo(obj) {
   this.imgUrl = obj.photos[0].image.web;
 }
 
+// constructor for nps gov (parks)
+function Parks(obj) {
+  this.name = obj.name;
+  this.description = obj.operatingHours[0].description
+}
+
 // function to grab api data from two separate sources and render them 
 // to the city.ejs file at the same time. 
 async function handler(req, res) {
@@ -42,33 +48,40 @@ async function handler(req, res) {
     limit: 5
   }
 
-  let teleportUrl = `https://api.teleport.org/api/urban_areas/slug:seattle/images/`;
+  let npsGovUrl = `https://developer.nps.gov/api/v1/parks?q=${search}&api_key=${process.env.NPS_GOV_API_KEY}&limit=5`
 
-  // if image does not exist, use this as default image
-  if (!teleportUrl) {
-    teleportUrl = `https://api.teleport.org/api/urban_areas/slug:seattle/images/`;
-  }
+  let teleportUrl = `https://api.teleport.org/api/urban_areas/slug:${search}/images/`;
 
   // gather api data from teleport api and yelp api
-  const [data, data2] = await Promise.all([
+  const [data, data2, data3] = await Promise.all([
     superagent.get(yelpUrl).set('Authorization', 'Bearer ' + process.env.YELP_API_KEY).query(queryParams),
-    superagent.get(teleportUrl)
+    superagent.get(teleportUrl),
+    superagent.get(npsGovUrl)
   ]).catch(err => console.log('error', err));
 
+  // compile returned data
+  let parkData = data3.body.data;
   let coords = data.body.region.center;
   let coordsArr = [coords.longitude, coords.latitude];
   let foodData = data.body.businesses;
 
   // create new instances
+  let parks = parkData.map(val => new Parks(val))
   let food = foodData.map(val => new Restaurant(val));
   let pic = new Photo(data2.body);
 
+  // console.log(parks);
+
+  // render all info to city.ejs page
   res.render('pages/city.ejs', {
     foodData: food,
     latLng: coordsArr,
-    cityPic: pic.imgUrl
+    cityPic: pic.imgUrl,
+    parkInfo: parks
   });
 }
 
 // export module
 module.exports.handler = handler;
+
+//TODO: create if logic for city images that do not have a url
